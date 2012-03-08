b6c79aee29a2b1e3760cae3d33106172f5ab9998
CHMB <christopherherwig@CHMBs-MacBook-Pro.local>
2012-03-07 23:41:14 -0500

    added personhood location file

---
+++ mapbox-streets/personhood.js
@@ -0,0 +1,94 @@
+var data = {
+    "type": "FeatureCollection",
+    "features": [
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point",
+            "coordinates": [-71.059773, 42.358431]    
+        },
+        "properties": {
+        	"name" : "Occupy Boston - Citizens United for Democracy"
+    	}
+    },
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point",
+            "coordinates": [-73.756232, 42.652579]
+    	}, 
+        "properties": {
+            "name" : "Occupy Albany - The Political Strategy Group	Albany, NY"
+        }
+    },    
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point",
+            "coordinates": [-78.476678, 38.029306]
+    	}, 
+        "properties": {
+            "name" : "Occupy Charlottesville, VA"
+        }
+    },            
+	{ 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point",
+            "coordinates": [19.733611, -155.092222]
+        }, 
+        "properties": {
+            "name" : "Occupy Philadelphia - Corporations Aren't People"
+        }
+    },            
+	{ 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point",
+            "coordinates": [-118.243685, 34.052234]
+        }, 
+        "properties": {
+            "name" : "Occupy Los Angeles & Move to Amend"
+        }
+    },            
+	{ 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point",
+            "coordinates": [-75.546589, 39.745947]
+    	}, 
+        "properties": {
+            "name" : "Occupy Delaware	Wilmington, DE"
+        }
+    },
+	{ 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point",
+            "coordinates": [-122.676207, 45.523452]
+    	}, 
+        "properties": {
+            "name" : "Occupy Portland - Anti-Corporate Personhood"
+        }
+	},            
+    { 
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point",
+            "coordinates": [-122.270803, 37.804372]
+    	}, 
+        "properties": {
+            "name" : "Occupy Oakland Courts"
+        }
+   	},         
+	{
+		"type": "Feature",
+		"geometry": { 
+            "type": "Point",
+            "coordinates": [-122.332071, 47.60621]
+    	}, 
+    	"properties": {
+            "name" : "Occupy Seattle - Get Money Out of Politics"
+        }
+	}]
+};
