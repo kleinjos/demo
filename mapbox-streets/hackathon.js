73f59011740c63c883b94c34981264f42be75d61
kleinjos <git@bf.33mail.com>
2012-03-14 22:49:55 -0400

    added map for occupy data site

--- mapbox-streets/hackathon.js
+++ mapbox-streets/hackathon.js
@@ -5,10 +5,10 @@ var data = {
         "type": "Feature",
         "geometry": { 
             "type": "Point",
-            "coordinates": [-77.03303, 38.905]
+            "coordinates": [-77.04295, 38.90536]
     	}, 
         "properties": {
-            "name" : "Occupy DC Corporate Personhood Solutions Working Group"
+            "name" : "Occupy Hackathon - Hosted at New Organizing Institute - 1850 M Street NW, Washington, DC 20036"
         }
 	}]
 };
